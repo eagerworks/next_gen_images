d67c548629b368e02d4f3ebae8601bef9d3977b9
JP Balarini <jp@eagerworks.com>
2022-11-09 18:57:36 -0300

    Add initial test

--- spec/next_gen_images_spec.rb
+++ spec/next_gen_images_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
+require 'next_gen_images/view_helpers'
+require 'rack'
 
-RSpec.describe NextGenImages do
-  it "has a version number" do
-    expect(NextGenImages::VERSION).not_to be nil
-  end
+module NextGenImages
+  RSpec.describe NextGenImages do
+    include ViewHelpers
+    # For Rack::Mime usage
+    include ::Rack
+    include ActionView::Helpers
+    include ActionView::Helpers::TagHelper
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it 'has a version number' do
+      expect(VERSION).not_to be nil
+    end
+
+    it 'creates a picture tag' do
+      expect(picture_tag('image.png')).to match(Regexp.new('\\A<picture>.*?</picture>\\z'))
+    end
   end
 end
