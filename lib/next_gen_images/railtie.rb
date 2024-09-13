d1d8efef94eb2d5b857f3bfdf4d42083e76bbaec
Juan Pablo Balarini <jpbalarini@gmail.com>
2024-09-13 17:39:55 -0300

    Do not include view helpers

--- lib/next_gen_images/railtie.rb
+++ lib/next_gen_images/railtie.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
-require 'next_gen_images/view_helpers'
-
 module NextGenImages
   # Load everything we need into rails
   class Railtie < Rails::Railtie
-    initializer 'next_gen_images.view_helpers' do
-      ActionView::Base.include ViewHelpers
-    end
-
     rake_tasks do
       path = File.expand_path(__dir__)
       Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
